45afdb40d4649fc487c374dc1d5c9912d5798e82
Jhon Esteban Herrera <estebanhz9919@gmail.com>
2021-11-18 20:48:57 -0500

    [Refactor]: Changes in the list of projects

--- src/data/projectList.js
+++ src/data/projectList.js
@@ -1,14 +1,22 @@
 const projectList = [
   {
     id: 1,
+    name: 'Countries App',
+    image: 'https://i.imgur.com/1uG93Tl.jpg',
+    stack: ['react', 'redux', 'sass', 'react router dom'],
+    code: 'https://github.com/JhonEsteban/Countries-App',
+    live: 'https://the-countries-now.vercel.app',
+  },
+  {
+    id: 2,
     name: 'React Crud App',
     image: 'https://i.imgur.com/jiGF0lB.png',
-    stack: ['react', 'hooks', 'sass', 'react router'],
+    stack: ['react', 'sass', 'react router dom'],
     code: 'https://github.com/JhonEsteban/React-Crud-App',
     live: 'https://react-reducer-app.netlify.app/login',
   },
   {
-    id: 2,
+    id: 3,
     name: 'Angular Crud App',
     image: 'https://i.imgur.com/GENk9Ju.png',
     stack: ['angular', 'boostrap5'],
@@ -16,7 +24,7 @@ const projectList = [
     live: 'https://jhonesteban.github.io/Angular-Crud',
   },
   {
-    id: 3,
+    id: 4,
     name: 'Todo App',
     image: 'https://i.imgur.com/3Jo33my.jpg',
     stack: ['html', 'sass', 'javascript'],
@@ -24,28 +32,20 @@ const projectList = [
     live: 'https://jhonesteban.github.io/Todo-App',
   },
   {
-    id: 4,
+    id: 5,
     name: 'Loopstudios',
     image: 'https://i.imgur.com/s2EmV7m.jpg',
-    stack: ['react', 'hooks', 'sass'],
+    stack: ['react', 'sass'],
     code: 'https://github.com/JhonEsteban/Loopstudios-Landing-Page',
     live: 'https://jhonesteban.github.io/Loopstudios-Landing-Page',
   },
   {
-    id: 5,
+    id: 6,
     name: 'Sunnyside Agency',
     image: 'https://i.imgur.com/dHXlOaM.jpg',
     stack: ['html', 'sass', 'javascript'],
     code: 'https://github.com/JhonEsteban/Sunnyside-Agency',
     live: 'https://jhonesteban.github.io/Sunnyside-Agency/src/index.html',
-  },
-  {
-    id: 6,
-    name: 'Insure Landing Page',
-    image: 'https://i.imgur.com/1ilMh62.jpg',
-    stack: ['html', 'sass', 'javascript'],
-    code: 'https://github.com/JhonEsteban/Insure-Landing-Page',
-    live: 'https://jhonesteban.github.io/Insure-Landing-Page/src/index.html',
   },
 ];
 
