5f7431b7846fab2e895a1e4f20307256e91b37fc
Jhon Esteban Herrera <estebanhz9919@gmail.com>
2021-11-18 21:39:36 -0500

    [Add]: DownLoadCvButton updated

--- src/components/downLoadCvButton/DownLoadCvButton.jsx
+++ src/components/downLoadCvButton/DownLoadCvButton.jsx
@@ -3,7 +3,7 @@ import './DownLoadCvButton.scss';
 const DownLoadCvButton = () => {
   return (
     <a
-      href='https://drive.google.com/file/d/1fmX76bQrLugTDX07Qo0_PJvh_JZTtmbm/view?usp=sharing'
+      href='https://drive.google.com/file/d/1wfiM_NLeVFYD9JOMZRUYkjoNRkleDl6v/view?usp=sharing'
       className='cv-button'
       target='_blank'
       rel='noreferrer'
